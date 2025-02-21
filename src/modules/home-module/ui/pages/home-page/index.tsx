e4f8d9d27953039727c772116a19190d1c16179f
HGSokol <g.w.sokolov98@mail.ru>
2025-02-22 01:22:20 +0300

    default settings

---
+++ src/modules/home-module/ui/pages/home-page/index.tsx
@@ -0,0 +1,11 @@
+import style from "./styles/light.module.scss";
+
+const HomePage = () => {
+  return (
+    <div className={style.homeWrapper}>
+      <div className={style.sectionContainer}>home</div>
+    </div>
+  );
+};
+
+export default HomePage;
